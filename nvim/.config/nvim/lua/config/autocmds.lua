fb0f46451e37804c715a98f176711edbb2792cf4
Ferdi <tech@ferdipret.com>
2026-03-21 16:11:38 +0000

    chore(nvim): format core config files

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,8 +1,8 @@
 vim.api.nvim_create_autocmd("BufEnter", {
-  pattern = "*.txt",
-  callback = function()
-    if vim.bo.filetype == "help" then
-      vim.cmd("wincmd L") -- Moves help window to the right (vertical split)
-    end
-  end,
+	pattern = "*.txt",
+	callback = function()
+		if vim.bo.filetype == "help" then
+			vim.cmd("wincmd L") -- Moves help window to the right (vertical split)
+		end
+	end,
 })
